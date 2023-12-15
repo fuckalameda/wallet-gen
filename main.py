4655a5bee51d0ff9a7fc4aa4d537311b57d3238e
Your Name <user@example.com>
2023-12-15 21:39:16 +0300

    feat: from any file support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from web3 import Web3
 import hashlib
 
@@ -8,9 +9,26 @@ def generate_wallet(seed):
     address = web3.eth.account.from_key(hashed_seed).address
     return str(hashed_seed), address
 
+def generate_wallet_from_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            seed = file.read().strip()
+            return generate_wallet(seed)
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        exit()
+
 if __name__ == "__main__":
-    input_string = input("Enter a string to generate wallet: ")
-    private_key, address = generate_wallet(input_string)
+    parser = argparse.ArgumentParser(description='Generate Ethereum wallet from any text or file.')
+    parser.add_argument('-f', '--file', help='Read seed from file')
+
+    args = parser.parse_args()
+
+    if args.file:
+        private_key, address = generate_wallet_from_file(args.file)
+    else:
+        input_string = input("Enter a string to generate wallet: ")
+        private_key, address = generate_wallet(input_string)
 
     print(f"\nPrivate Key: {private_key}")
     print(f"Address: {address}")
